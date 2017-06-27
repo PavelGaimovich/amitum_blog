5fd6ca735bc3884e05564fb96df1c843955dca68
Pavel Gaimovich <pavels.gaimovics@gmail.com>
2017-06-27 14:22:43 +0300

    Added slug-url

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,16 @@ class Article < ApplicationRecord
   belongs_to :user
 
   paginates_per 5
+
+  def to_param
+    [title.parameterize].join("-")
+  end
+
+  def self.find(input)
+    if input.is_a?(Integer)
+      super
+    else
+      find_by_title(input)
+    end
+  end
 end
